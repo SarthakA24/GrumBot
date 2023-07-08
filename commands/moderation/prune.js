eb67cc183813598fa2b0acd0f2eee641df8f4396
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2023-07-08 22:53:11 +0530

    updated prune command to act to admins only

--- commands/moderation/prune.js
+++ commands/moderation/prune.js
@@ -6,16 +6,19 @@ module.exports = {
 		.setDescription('Prune up to 99 messages.')
 		.addIntegerOption(option => option.setName('amount').setDescription('Number of messages to prune')),
 	async execute(interaction) {
-		const amount = interaction.options.getInteger('amount');
-
+		if (interaction.member.roles.cache.some(role => role.name === 'Admin')) {
+			const amount = interaction.options.getInteger('amount');
 		if (amount < 1 || amount > 99) {
-			return interaction.reply({ content: 'You need to input a number between 1 and 99.', ephemeral: true });
+			return interaction.reply({ content: 'You need to input a number between 1 and 99.', ephemeral: false });
 		}
 		await interaction.channel.bulkDelete(amount, true).catch(error => {
 			console.error(error);
-			interaction.reply({ content: 'There was an error trying to prune messages in this channel!', ephemeral: true });
+			interaction.reply({ content: 'There was an error trying to prune messages in this channel!', ephemeral: false });
 		});
 
-		return interaction.reply({ content: `Successfully pruned \`${amount}\` messages.`, ephemeral: true });
+		return interaction.reply({ content: `Successfully pruned \`${amount}\` messages.`, ephemeral: false });
+		} else {
+			return interaction.reply({ content: 'You do not have permission to use this command.', ephemeral: false });
+		}
 	},
 };
