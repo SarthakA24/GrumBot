c346f327fc4617023f01223e8c82b365fac0ec75
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2023-09-24 00:49:29 +0530

    updated /ping reply

--- commands/fun/ping.js
+++ commands/fun/ping.js
@@ -5,6 +5,6 @@ module.exports = {
 		.setName('ping')
 		.setDescription('Checks if GrumBot is Online or not!'),
 	async execute(interaction) {
-		await interaction.reply('Beep Boop Beep Beep! Hi, I am GrumBot!');
+		await interaction.reply('Beep Boop Beep Beep! Hi, I am Grumbot, the bot for the EthelMC Discord Server. I was created by <@373775406148616192>. I am still in development, so please be patient with me.');
 	},
 };
