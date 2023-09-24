b89df5d6fc5f873a0afe4ab5b544345f7c32a146
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2023-09-24 11:40:35 +0530

    added missing require statement

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const fs = require('node:fs');
 const path = require('node:path');
 const { Client, Collection, Events, GatewayIntentBits, ActivityType, GatewayVersion, EmbedBuilder } = require('discord.js');
 const { token } = require('./config.json');
+const { request } = require('request');
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
 
