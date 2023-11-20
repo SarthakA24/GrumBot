e35acc1bdea23f49f7e6aa06450ab6ec27afbba8
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2023-11-20 17:23:29 +0530

    added missing import statements

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fs = require('node:fs');
 const path = require('node:path');
-const { Client, Collection, Events, GatewayIntentBits, ActivityType, GatewayVersion, EmbedBuilder } = require('discord.js');
+const { Client, Collection, Events, GatewayIntentBits, ActivityType, GatewayVersion, EmbedBuilder, ButtonBuilder, ActionRowBuilder } = require('discord.js');
 const { token } = require('./config.json');
 const request = require('request');
 
