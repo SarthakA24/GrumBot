6e832bc82d2100426b5045d984cc2cf8e23ffb7a
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2023-11-21 16:08:33 +0530

    added missing import statement

--- commands/server/vote.js
+++ commands/server/vote.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder, ButtonBuilder, ActionRowBuilder, ButtonStyle } = require('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
